596cb8e16f4f55f409b01c894768352bafa2bd3b
Zain Ali <z.a.cheema7777@gmail.com>
2020-08-15 20:20:48 +0500

    Modified User.php: changed insert methods query from INSERT IGNORE to INSERT

--- classes/model/User.php
+++ classes/model/User.php
@@ -3,7 +3,7 @@ include_once "/opt/lampp/htdocs/packages.php";
 
 class User extends DBEcommerceConn {   
     public function insert($uid, $email, $fullname, $imgdir) {
-        $sql  = "INSERT IGNORE INTO users(uid, email, fullname, img_dir) VALUES (?, ?, ?, ?)";
+        $sql  = "INSERT INTO users(uid, email, fullname, img_dir) VALUES (?, ?, ?, ?)";
         $stmt = $this->connection()->prepare($sql);
         if ($stmt) return $stmt->execute([$uid, $email, $fullname]);
         return false;
