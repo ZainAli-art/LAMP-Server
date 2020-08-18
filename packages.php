8033cb1f379f32a8a4608cf65e0d9446386a8fe9
Zain Ali <z.a.cheema7777@gmail.com>
2020-08-18 11:43:26 +0500

    deleted Customers table and all links and other deprecated files

--- packages.php
+++ packages.php
@@ -10,18 +10,15 @@ include_once $includesPath . "DbEcommerceConn.php";
 
 // model paths
 include_once $modelPath . "Category.php";
-include_once $modelPath . "Customer.php";
 include_once $modelPath . "Product.php";
 include_once $modelPath . "User.php";
 
 // view paths
 include_once $viewPath . "CategoryView.php";
-include_once $viewPath . "CustomerView.php";
 include_once $viewPath . "ProductView.php";
 include_once $viewPath . "UserView.php";
 
 // controller paths
 include_once $controllerPath . "CategoryController.php";
-include_once $controllerPath . "CustomerController.php";
 include_once $controllerPath . "ProductController.php";
 include_once $controllerPath . "UserController.php";
